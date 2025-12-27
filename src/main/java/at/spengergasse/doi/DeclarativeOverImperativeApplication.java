e27fab8e075abf5446f4dd63b58264aede1b341c
Sima Baric <sima.baric0605@gmail.com>
2025-12-27 23:16:15 +0100

    init project

---
+++ src/main/java/at/spengergasse/doi/DeclarativeOverImperativeApplication.java
@@ -0,0 +1,13 @@
+package at.spengergasse.doi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeclarativeOverImperativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeclarativeOverImperativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/spengergasse/doi/DeclarativeOverImperativeApplicationTests.java
@@ -0,0 +1,13 @@
+package at.spengergasse.doi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeclarativeOverImperativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
